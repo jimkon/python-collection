13e2b3715dcf2e427281c81a2c7716585ad2a783
jimkon <dkontzedakis@gmail.com>
2022-11-20 21:35:12 +0000

    packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+   name='python_collection',
+   version='0.0',
+   description='Collection of useful python code segments',
+   packages=['python_collection'],  #same as name
+)
