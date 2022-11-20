a67daded1a18262adcda96a605ef919c1bcaf421
jimkon <dkontzedakis@gmail.com>
2022-11-20 21:30:10 +0000

    html pages toolkit

---
+++ toolkit/html_pages/html_pages.py
@@ -0,0 +1,113 @@
+import abc
+import base64
+import os
+import tempfile
+import matplotlib.pyplot as plt
+
+_TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__name__)), 'templates')
+
+
+def _read_template(template_name):
+    filename = os.path.join(_TEMPLATE_DIR, template_name+'.html')
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+class HTMLObject(abc.ABC):
+    @abc.abstractmethod
+    def html(self) -> str:
+        return None
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            f.write(self.html())
+
+
+class HTMLBuilder(HTMLObject):
+    def __init__(self, template_str):
+        self._html_str = template_str
+
+    def replace(self, this, with_that):
+        self._html_str = self._html_str.replace(f"[{this}]", with_that)
+
+    def html(self):
+        return self._html_str
+
+
+class HTMLPage(HTMLBuilder):
+    def __init__(self):
+        super().__init__('')
+
+    def add_element(self, element):
+        self._html_str = f"{self._html_str}\n<div>{element}</div>"
+        return self
+
+
+class TemplateHTMLBuilder(HTMLBuilder):
+    def __init__(self, template_name):
+        template_html = _read_template(template_name)
+        super().__init__(template_html)
+
+
+class ImageHTML(TemplateHTMLBuilder):
+    def __init__(self, img_bytes, width=None, height=None):
+        super().__init__('image')
+        self.replace('image', self._bytes_to_utf8(img_bytes))
+
+        if height:
+            self.replace('height', f'height="{height}"')
+        else:
+            self.replace('height', '')
+
+        if width:
+            self.replace('width', f'width="{width}"')
+        else:
+            self.replace('width', '')
+
+    @staticmethod
+    def _bytes_to_utf8(_bytes):
+        return base64.b64encode(_bytes).decode("utf-8")
+
+    @staticmethod
+    def from_image_file(image_path):
+        with open(image_path, "rb") as image_file:
+            return ImageHTML(image_file.read())
+
+    @staticmethod
+    def from_matplotlib():
+        tmp_file = tempfile.NamedTemporaryFile(suffix='_image.png', delete=False)
+        plt.savefig(tmp_file.name)
+
+        image_html = ImageHTML.from_image_file(tmp_file.name)
+
+        tmp_file.close()
+        os.unlink(tmp_file.name)
+
+        return image_html
+
+
+class TabsHTML(TemplateHTMLBuilder):
+    def __init__(self):
+        super().__init__('tabs')
+        self._add_default_id = True
+
+    def _add_button(self, label):
+        default_id = "id=\"defaultOpen\"" if self._add_default_id else ""
+        self._add_default_id = False
+        to_add = f"<button class=\"tablinks\" onclick=\"openCity(event, '{label}')\" {default_id}>{label}</button>\n\t[button]"
+        self.replace('button', to_add)
+
+    def _add_div(self, label, html_content):
+        to_add = f"<div id=\"{label}\" class=\"tabcontent\">\n{html_content}\n</div>\n[div_tab]"
+        self.replace('div_tab', to_add)
+
+    def add_tab(self, label, html_content):
+        self._add_button(label)
+        self._add_div(label, html_content)
+        return self
+
+    def html(self):
+        self.replace("[button]", '')
+        self.replace("[div_tab]", '')
+        return self._html_str
+
---
+++ toolkit/html_pages/test_html_pages.py
@@ -0,0 +1,70 @@
+import unittest
+from unittest.mock import patch, mock_open
+
+import html_pages
+
+
+class TestHtml(unittest.TestCase):
+    @patch("html_pages._TEMPLATE_DIR", "template_dir")
+    def test__read_template(self):
+        with patch("builtins.open", mock_open(read_data="data")) as mock_file:
+            self.assertEqual(html_pages._read_template('test_template'), "data")
+        mock_file.assert_called_with('template_dir\\test_template.html', 'r')
+
+
+class TestHTMLBuilder(unittest.TestCase):
+    def test_builder(self):
+        builder = html_pages.HTMLBuilder("random_text [replace1] [replace2] [replace1] [replace3]")
+        builder.replace('replace1', 'value1')
+        builder.replace('replace2', 'value2')
+        builder.replace('replace3', 'value3')
+
+        html = builder.html()
+        expected_html = "random_text value1 value2 value1 value3"
+        self.assertEqual(html, expected_html)
+
+
+class TestHTMLPage(unittest.TestCase):
+    def test_page_builder(self):
+        page = html_pages.HTMLPage()
+
+        page.add_element('test1')
+        page.add_element('test2')
+        page.add_element('test3')
+
+        html = page.html()
+        expected_html = "\n<div>test1</div>\n<div>test2</div>\n<div>test3</div>"
+
+        self.assertEqual(html, expected_html)
+
+
+class TestTemplateHTMLBuilder(unittest.TestCase):
+    def test_from_template_file(self):
+        with patch("builtins.open", mock_open(read_data="template_file_html")) as mock_file:
+            builder = html_pages.TemplateHTMLBuilder("test_template")
+            self.assertEqual(builder.html(), "template_file_html")
+
+
+class TestImageHTML(unittest.TestCase):
+    @patch.object(html_pages.ImageHTML, '_bytes_to_utf8', return_value='_bytes_to_utf8')
+    def test_no_size(self, mock_conv):
+        img_html = html_pages.ImageHTML(b"test_bytes").html()
+        mock_conv.assert_called_with(b"test_bytes")
+        self.assertEqual(img_html, '<img src="data:image/gif;base64, _bytes_to_utf8" >')
+
+    @patch.object(html_pages.ImageHTML, '_bytes_to_utf8', return_value='_bytes_to_utf8')
+    def test_height(self, mock_conv):
+        img_html = html_pages.ImageHTML(b"test_bytes", height=200).html()
+        mock_conv.assert_called_with(b"test_bytes")
+        self.assertEqual(img_html, '<img src="data:image/gif;base64, _bytes_to_utf8" height="200">')
+
+    @patch.object(html_pages.ImageHTML, '_bytes_to_utf8', return_value='_bytes_to_utf8')
+    def test_width(self, mock_conv):
+        img_html = html_pages.ImageHTML(b"test_bytes", width=200).html()
+        mock_conv.assert_called_with(b"test_bytes")
+        self.assertEqual(img_html, '<img src="data:image/gif;base64, _bytes_to_utf8" width="200">')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
