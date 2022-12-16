1840c38775d4d70ba8ed50b5223487a385e5f361
jimkon <dkontzedakis@gmail.com>
2022-12-16 21:35:53 +0000

    func_execution_logging

--- my_toolkit/misc/logs.py
+++ my_toolkit/misc/logs.py
@@ -1,5 +1,8 @@
-from src.configs import VERBOSITY
+import logging
+from functools import wraps
+from time import time
 
+VERBOSITY = 1
 
 # https://www.codegrepper.com/code-examples/python/python+print+error+in+red
 def colored(r, g, b, text):
@@ -10,6 +13,40 @@ def log(*args, verbosity=1, rgb=(255, 255, 255)):
     if verbosity <= VERBOSITY:
         colored_args = tuple([colored(*rgb, arg) for arg in args])
         print(*colored_args)
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+_CNT = 0
+
+
+def func_execution_logging(_func):
+    @wraps(_func)
+    def wrapper(*args, **kwargs):
+
+        start_time = time()
+        _func_name = f"{_func.__module__}.{_func.__qualname__}"
+
+        global _CNT
+        padding = '--' * _CNT
+        _CNT += 1
+        try:
+            logger.info(f"{padding}=> Running {_func_name}...")
+            res = _func(*args, **kwargs)
+        except Exception as e:
+            _CNT -= 1
+            logger.error(f"Function {_func_name} raised exception: '{e}'.")
+            raise
+
+        elapsed_time = time() - start_time
+        logger.info(
+            f"<={padding} Function {_func_name} successfully finished in {elapsed_time:.2f} seconds."
+        )
+        _CNT -= 1
+        return res
+
+    return wrapper
 
 
 if __name__=="__main__":
