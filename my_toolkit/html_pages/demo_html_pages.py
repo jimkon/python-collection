c74a53f879e0f28559b3948f5ee4d9071ce93ab3
jimkon <dkontzedakis@gmail.com>
2022-12-20 18:56:08 +0000

    extend demo

--- my_toolkit/html_pages/demo_html_pages.py
+++ my_toolkit/html_pages/demo_html_pages.py
@@ -12,10 +12,11 @@ tabs.add_tab("Tab 1", "Example text")
 tabs.add_tab("Tab 2", html_pages.SimpleHTMLTable(
     pd.DataFrame(data={'c1': [1, 2, 3, 4], 'c2': [4, 3, 2, 1], 'c3': [2, 1, 3, 4]})))
 
+
 sub_tabs = html_pages.TabsHTML()
 sub_tabs.add_tab("Sub tab 1", "Sub tab 1")
 sub_tabs.add_tab("Sub tab 2", "Sub tab 2")
-tabs.add_tab("Tab 3", sub_tabs)
+tabs.add_tab("Tab 3", html_pages.HTMLPage().add_element("<h2>Sub tabs</h2>").add_element(sub_tabs))
 
 page.add_element(tabs)
 
